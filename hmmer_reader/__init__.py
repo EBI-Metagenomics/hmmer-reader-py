bc6eac35373148704653c60e1e8240462f1d75e3
Danilo Horta <danilo.horta@pm.me>
2020-01-09 22:31:45 +0000

    0.0.2

--- hmmer_reader/__init__.py
+++ hmmer_reader/__init__.py
@@ -2,6 +2,6 @@ from ._cli import cli
 from ._reader import HMMERParser, HMMERProfile, open_hmmer
 from ._testit import test
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 __all__ = ["__version__", "open_hmmer", "test", "cli", "HMMERParser", "HMMERProfile"]
